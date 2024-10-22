4ea4a860ac50a4172587e041c6511c6f347638c4
KevinHolidee <kevin@holidee.fr>
2024-10-22 16:14:07 +0200

    Image Halloween

---
+++ tilesets/Haloween.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Halloween" tilewidth="32" tileheight="32" tilecount="960" columns="16">
+ <image source="Halloween/Haloween.png" width="512" height="1920"/>
+</tileset>
---
+++ tilesets/WA-Sol.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="WA-Sol" tilewidth="32" tileheight="32" tilecount="8480" columns="106">
+ <image source="Halloween/WA-Sol.png" width="3392" height="2560"/>
+</tileset>
